41ef8f49aa8cacfa37519d822358b0fdde8d27af
Mohamed-Ashraf <140234307+MohammedAshrf@users.noreply.github.com>
2024-08-25 18:01:04 +0300

    update files.1

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,71 +1,159 @@
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import GoBackArrow from "../assets/SVGs/GoBackArrow";
+import { Tooltip, Button } from "@material-tailwind/react";
 
 export default function SingleProduct() {
   const { data } = useSelector((state) => state.products);
   const { category, id } = useParams();
   //   console.log(params);
+
+  const theSingleProduct = data
+    .filter((product) => product.type === category)
+    .find((product) => product.id === id);
+  console.log(theSingleProduct);
+
   const navigate = useNavigate();
 
   return (
     <>
       <div>
         <div
-          className="mx-8 my-2 py-1 px-2 flex gap-1 cursor-pointer
+          className="mx-6 my-2 mb-4 py-1 px-2 flex gap-1 cursor-pointer
                     bg-gray-200 hover:bg-gray-300 w-fit rounded-full"
           onClick={() => navigate(`../products/${category}`)}
         >
           {GoBackArrow} Back to all products
         </div>
-        <div className="px-8 flex flex-wrap gap-12 items-center justify-center">
-          {data
-            .filter((product) => product.type === category)
-            .filter((product) => product.id === id)
-            .map((product) => {
-              return (
-                <div
-                  key={product.id}
-                  className="flex flex-col basis-1/5
-                  border-black border rounded-md"
-                >
-                  <div
-                    className="w-64 h-48 mb-4 rounded-lg overflow-hidden 
-                  hover:shadow-xl m-2 cursor-pointer"
-                    onClick={() => console.log("singleProduct")}
-                  >
-                    <img
-                      className="w-full h-full"
-                      src={product.img}
-                      alt={product.name}
-                    />
-                  </div>
-                  <h2 className="text-center text-xl mb-1 font-bold">
-                    {product.name}
-                  </h2>
-                  <p className="text-center text-sm px-3 py-1 text-gray-600 font-normal">
-                    {product.text}
-                  </p>
-                  <hr className="border" />
-                  <div className="flex justify-between p-2 text-gray-600">
-                    <p>{product.price}$</p>
-                    <div className="flex gap-2">
-                      {product.color.map((c) => {
-                        {
+        {/* <div id="try">
+          {theSingleProduct && <div>{theSingleProduct.img}</div>}
+        </div> */}
+        <div className="px-12 flex flex-wrap gap-12 items-center  justify-center">
+          {theSingleProduct && (
+            <div
+              key={theSingleProduct.id}
+              className="flex border-black border rounded-md"
+              style={{ gap: "8%" }}
+            >
+              <div
+                className="w-1/3 mb-4 rounded-lg overflow-hidden 
+                  shadow-2xl m-2"
+                onClick={() => console.log("singleProduct")}
+              >
+                <img
+                  className="w-full h-full"
+                  src={theSingleProduct.img}
+                  alt={theSingleProduct.name}
+                />
+              </div>
+              <div className="w-1/2 flex flex-col items-start justify-center gap-2">
+                <h2 className="text-center text-xl mb-1 font-bold">
+                  {theSingleProduct.name}
+                </h2>
+                <p className="font-bold text-red-800">15% OFF</p>
+                <p className="text-start text-sm text-gray-600 font-normal">
+                  {theSingleProduct.text}
+                </p>
+                {theSingleProduct.size && (
+                  <div className="w-full max-w-sm min-w-[200px]">
+                    <label className="block mb-1 text-sm text-slate-800">
+                      Pick a size
+                    </label>
+                    <div className="relative">
+                      <select
+                        className="w-full h-10 bg-transparent 
+                                placeholder:text-slate-400 text-slate-700
+                                text-sm border border-slate-200 rounded px-3
+                                py-2 transition duration-300 ease focus:outline-none
+                                focus:border-slate-400 hover:border-slate-400
+                                shadow-sm focus:shadow-md appearance-none
+                                cursor-pointer"
+                      >
+                        {theSingleProduct.size.map((size) => {
                           return (
-                            <div
-                              key={c}
-                              style={{ backgroundColor: `${c}` }}
-                              className="rounded-full w-5 h-5"
-                            ></div>
+                            <option key={size} value={size}>
+                              {size}
+                            </option>
                           );
-                        }
-                      })}
+                        })}
+                      </select>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.2"
+                        stroke="currentColor"
+                        className="h-5 w-5 ml-1 absolute top-2.5 right-2.5 
+                          text-slate-700"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
+                        />
+                      </svg>
                     </div>
                   </div>
+                )}
+
+                <div className="w-full max-w-sm min-w-[200px]">
+                  <label className="block mb-1 text-sm text-slate-800">
+                    Select a Color
+                  </label>
+                  <div className="relative">
+                    <select
+                      className="w-full h-10 bg-transparent 
+                                placeholder:text-slate-400 text-slate-700
+                                text-sm border border-slate-200 rounded px-3
+                                py-2 transition duration-300 ease focus:outline-none
+                                focus:border-slate-400 hover:border-slate-400
+                                shadow-sm focus:shadow-md appearance-none
+                                cursor-pointer"
+                    >
+                      {theSingleProduct.color.map((color) => {
+                        return (
+                          <option key={color} value={color}>
+                            {color}
+                          </option>
+                        );
+                      })}
+                    </select>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.2"
+                      stroke="currentColor"
+                      className="h-5 w-5 ml-1 absolute top-2.5 right-2.5 
+                          text-slate-700"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
+                      />
+                    </svg>
+                  </div>
                 </div>
-              );
-            })}
+
+                <Tooltip
+                  content="Add to cart"
+                  placement="bottom"
+                  color="black"
+                  variant="outlined"
+                >
+                  <Button
+                    color="black"
+                    variant="outlined"
+                    size="md"
+                    ripple={true}
+                  >
+                    Add to cart
+                  </Button>
+                </Tooltip>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
