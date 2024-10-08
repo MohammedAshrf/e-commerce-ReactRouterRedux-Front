9e74c35d2146d9cde4deeb637c1c922577796ee9
Mohamed-Ashraf <140234307+MohammedAshrf@users.noreply.github.com>
2024-10-08 17:55:32 +0300

    update changes

--- src/components/auth/IsAuthorized.jsx
+++ src/components/auth/IsAuthorized.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
-export default function IsAuthorized() {
+export function IsAuthorized() {
   const { user } = useSelector((state) => state.auth);
   const location = useLocation();
 
